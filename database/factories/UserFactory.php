f7e613c69fad3389aa59a755e7c34f9bdbf58356
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-19 16:29:15 -0500

    Add user controller and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,11 +22,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $first_name = $this->faker->unique()->firstName;
+        $last_name = $this->faker->unique()->lastName;
+        $username = Str::lower(trim($first_name . "." . $last_name));
+        $domain = '@divisions.com';
+
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => "$first_name $last_name",
+            'email' => "$username$domain",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
