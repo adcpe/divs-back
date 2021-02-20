559b30f83cb54fa47d282bc49082fd1ef4513458
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-20 16:56:21 -0500

    Added route to view children of Divisions

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DivisionController;
 
+use App\Models\Division;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,6 +21,11 @@ use App\Http\Controllers\DivisionController;
 
 Route::apiResource('users', UserController::class);
 Route::apiResource('divisions', DivisionController::class);
+
+Route::get('/divisions/{id}/children', function ($id) {
+  $name = Division::find($id)->name;
+  return DB::table('divisions')->where('parent', '=', $name)->select('name')->get();
+});
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
